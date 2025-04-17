f8f8db69dc66188705c4c9ca792678ac505c8b2e
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-17 17:42:57 -0300

    update: parkings/admin.py added formfield_for_foreignkey() to change our queryset to just show in the Register Admin panel only the empty parking spots in our ParkingRecordAdmin class.

--- parkings/admin.py
+++ parkings/admin.py
@@ -14,3 +14,8 @@ class ParkingRecordAdmin(admin.ModelAdmin):
     list_display = ['vehicle', 'parking_spot', 'entry_time', 'exit_time']
     search_fields = ['vehicle__license_plate', 'parking_spot__spot_number']  #PARKING_SPOT__SPOT_NUM serve para resolver a FK
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'parking_spot' and not request.resolver_match.url_name.endswith('change'):
+            kwargs['queryset'] = ParkingSpot.objects.filter(is_empty=True)
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
