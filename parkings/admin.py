f9d9ac70066415596b3c674d0e65cbd0a2c22361
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-17 17:13:10 -0300

    update: parkings/admin.py added two new Admin Class to register those table (ParkingSpot and ParkingRecords). update: vehicles/admin.py added two new Admin Class to register those table (VehicleType and Vehicle) and changed it verbose_name in vehicles/apps.py.

--- parkings/admin.py
+++ parkings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from parkings.models import ParkingRecord, ParkingSpot
 
-# Register your models here.
+
+@admin.register(ParkingSpot)
+class ParkingSpotAdmin(admin.ModelAdmin):
+    list_display = ['spot_number', 'is_empty']
+    search_fields = ['spot_number']
+    list_filter = ['is_empty']
+
+
+@admin.register(ParkingRecord)
+class ParkingRecordAdmin(admin.ModelAdmin):
+    list_display = ['vehicle', 'parking_spot', 'entry_time', 'exit_time']
+    search_fields = ['vehicle__license_plate', 'parking_spot__spot_number']  #PARKING_SPOT__SPOT_NUM serve para resolver a FK
+
--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from vehicles.models import VehicleType, Vehicle
 
-# Register your models here.
+
+@admin.register(VehicleType)
+class VehicleTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['license_plate', 'brand', 'model', 'color', 'owner']
+    search_fields = ['license_plate', 'brand', 'model', 'owner']
+    list_filter = ['vehicle_type']
