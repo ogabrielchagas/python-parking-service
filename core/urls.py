655c9d2ac0320c4ce17f7fcfba438f4a04a0df6c
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-23 16:15:50 -0300

    add: new app authentication created, and authentication/urls.py added our endpoints for token, token refresh and token verify. update: new endpoint in core/urls.py to authentication.urls.

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('customers.urls')),
     path('api/v1/', include('parkings.urls')),
     path('api/v1/', include('vehicles.urls')),
