1ec104044de2b5405fce37a07974e52b900b3790
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-23 14:32:32 -0300

    update: added new serializer view CustomerViewSet in customers/views.py. add: new file customers/serializers.py and added a new serializer CustomerSerializer for Customer Model.

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from customers.models import Customer
 
-# Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    ...
