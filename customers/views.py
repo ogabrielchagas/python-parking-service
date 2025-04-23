0722c47d7571402dbec637486a52f87ff45aa4e4
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-23 14:58:56 -0300

    update: finished our viewset CustomerViewSet with queryset and serializer_class, than tested our endpoint in Postman, all workedgit add .

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from customers.models import Customer
+from customers.serializers import CustomerSerializer
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
-    ...
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
