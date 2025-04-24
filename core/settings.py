f134653b64fb3a931f5d655f023b7c8d76afac11
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-24 17:45:31 -0300

    update: changed our DB connections in core/settings.py at DATABASES, changed to postgresql, and removed dbsqlite3.

--- core/settings.py
+++ core/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'parking_service',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'parking_db',
+        'PORT': '5432',
     }
 }
 
