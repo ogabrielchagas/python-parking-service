35e9c283a5ff94b710f63db84dbbae39dc16c8fd
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-24 13:18:18 -0300

    update: changed endpoint /admin to our root endpoint localhost:8000. It must be the last path in core/urls.py due to Django and Python regex to deal with endpoints.

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('customers.urls')),
     path('api/v1/', include('parkings.urls')),
     path('api/v1/', include('vehicles.urls')),
+
+    path('', admin.site.urls),  #URL root deve sempre vir por ultimo
 ]
